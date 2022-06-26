c44851efb58b0fd0e76e8c04d6b6877317c97ced
trotyl <mail@trotyl.xyz>
2022-06-25 23:41:06 -0400

    remove py-bcrypt

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(20), nullable=False, unique=True)
-    password=db.Column(db.String(80), nullable=False)
+    password=db.Column(db.String(255), nullable=False)
 
 class RegisterForm(FlaskForm):
     username=StringField(validators=[InputRequired(), Length(
